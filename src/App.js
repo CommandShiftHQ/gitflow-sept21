126b664475f92a415d565663c4f0ce7d45863098
Heli Devine <heli.devine@gmail.com>
2022-02-22 21:27:33 +0000

    changes after codereview

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <main>
-          <h2>Git Pull practice page</h2>
-  <h4>Git branching</h4>
-  <p>Tuesday's lesson- breakout room 1</p>
-        </main>
         <h1 className="App-header-title">My website</h1>
       </header>
+      <main>
+        <h2>Git Pull practice page</h2>
+        <h4>Git branching</h4>
+        <p>Tuesday's lesson- breakout room 1</p>
+      </main>
     </div>
   );
 }
