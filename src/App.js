01467598bb8ef217f7eea2ca17ccb948c9e9ca1f
Heli Devine <90283555+heliDevine@users.noreply.github.com>
2022-02-22 21:22:51 +0000

    Update src/App.js
    Co-authored-by: Dave Taylor <90399346+Daveoxys@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
       <header className="App-header">
         <main>
           <h2>Git Pull practice page</h2>
-          <p>Tuesday's lesson- breakout room 1</p>
+  <h4>Git branching</h4>
+  <p>Tuesday's lesson- breakout room 1</p>
         </main>
         <h1 className="App-header-title">My website</h1>
       </header>
