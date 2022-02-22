de5fb27aa64f197f60b07aa631e89b6b0016ad95
Heli Devine <90283555+heliDevine@users.noreply.github.com>
2022-02-22 21:22:29 +0000

    Update src/App.js
    Co-authored-by: Nathan Horne <32276550+naylfc90@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <main>
-          <h1>Git Pull practice page</h1>
+          <h2>Git Pull practice page</h2>
           <p>Tuesday's lesson- breakout room 1</p>
         </main>
         <h1 className="App-header-title">My website</h1>
